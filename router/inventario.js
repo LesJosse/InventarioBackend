98d6522e0c81d5013d160757a0670fa1fb0f3a9b
LesJosse <josajosa97@outlook.com>
2022-12-04 13:09:02 -0500

    agregando cambios de validación al inventario

--- router/inventario.js
+++ router/inventario.js
@@ -3,8 +3,9 @@ const Inventario = require("../models/Inventario");
 const { validateInventory } = require("../helpers/validar");
 const { findById } = require("../models/Inventario");
 const router = Router();
-const {validarJWT} = require('../middlewares/validar-jwt');
-const {validarRolAdmin} = require('../middlewares/validar-rol')
+const { validationResult, check } = require("express-validator");
+const { validarJWT } = require("../middlewares/validar-jwt");
+const { validarRolAdmin } = require("../middlewares/validar-rol");
 
 router.get("/", [validarJWT], async (req, res) => {
   try {
@@ -34,103 +35,118 @@ router.get("/", [validarJWT], async (req, res) => {
     res.status(500).send("Ocurrió un error");
   }
 });
-router.post("/", [validarJWT, validarRolAdmin], async (req, res) => {
-  try {
-    const validations = validateInventory(req);
-    if (validations.length > 0) {
-      return res.status(400).send(validations);
-    }
-    console.log("Recibiendo objeto tipoEquipo con exito", req.body);
-    //Primero se verifica si existe un serial
-    const exitsInventorySerial = await Inventario.findOne({
-      serial: req.body.serial,
-    });
-    if (exitsInventorySerial) {
-      return res.status(400).send("El serial existe para otro equipo");
-    }
+router.post(
+  "/",
+  [
+    validarJWT,
+    validarRolAdmin
+  ],
+  async (req, res) => {
+    try {
+      const validations = validateInventory(req);
+      if (validations.length > 0) {
+        return res.status(400).send(validations);
+      }
+      console.log("Recibiendo objeto tipoEquipo con exito", req.body);
+      //Primero se verifica si existe un serial
+      const exitsInventorySerial = await Inventario.findOne({
+        serial: req.body.serial,
+      });
+      if (exitsInventorySerial) {
+        return res.status(400).send("El serial existe para otro equipo");
+      }
+      let inventory = new Inventario();
+      inventory.serial = req.body.serial;
+      inventory.model = req.body.model;
+      inventory.description = req.body.description;
+      inventory.photoComputer = req.body.photoComputer;
+      inventory.color = req.body.color;
+      inventory.datePurchase = req.body.datePurchase;
+      inventory.price = req.body.price;
 
-    let inventory = new Inventario();
-    inventory.serial = req.body.serial;
-    inventory.model = req.body.model;
-    inventory.description = req.body.description;
-    inventory.photoComputer = req.body.photoComputer;
-    inventory.color = req.body.color;
-    inventory.datePurchase = req.body.datePurchase;
-    inventory.price = req.body.price;
-    inventory.userCharge = req.body.userCharge._id;
-    inventory.brand = req.body.brand._id;
-    inventory.computerStatus = req.body.computerStatus._id;
-    inventory.computerType = req.body.computerType._id;
-    inventory.creationDate = new Date();
-    inventory.updateDate = new Date();
+      inventory.userCharge = req.body.userCharge._id;
+      inventory.brand = req.body.brand._id;
+      inventory.computerStatus = req.body.computerStatus._id;
+      inventory.computerType = req.body.computerType._id;
+      inventory.creationDate = new Date();
+      inventory.updateDate = new Date();
 
-    inventory = await inventory.save();
-    res.send(inventory);
-    console.log("Creando nuevo inventario: ", inventory);
-  } catch (e) {
-    console.log(e);
-    res.status(500).send("Ocurrió un error");
+      inventory = await inventory.save();
+      res.send(inventory);
+      console.log("Creando nuevo inventario: ", inventory);
+    } catch (e) {
+      console.log(e);
+      res.status(500).send("Ocurrió un error");
+    }
   }
-});
-router.put("/:inventarioId", [validarJWT, validarRolAdmin], async (req, res) => {
-  try {
-    const validations = validateInventory(req);
-    if (validations.length > 0) {
-      return res.status(400).send(validations);
-    }
+);
+router.put(
+  "/:inventarioId",
+  [validarJWT, validarRolAdmin],
+  async (req, res) => {
+    try {
+      const validations = validateInventory(req);
+      if (validations.length > 0) {
+        return res.status(400).send(validations);
+      }
 
-    let inventory = await Inventario.findById(req.params.inventarioId);
-    if (!inventory) {
-      return res.status(400).send("El inventario no existe");
-    }
-    const exitsInventorySerial = await Inventario.findOne({
-      serial: req.body.serial,
-      _id: { $ne: inventory._id },
-    });
-    if (exitsInventorySerial) {
-      return res.status(400).send("El serial existe para otro equipo");
-    }
-    inventory.serial = req.body.serial;
-    inventory.model = req.body.model;
-    inventory.description = req.body.description;
-    inventory.photoComputer = req.body.photoComputer;
-    inventory.color = req.body.color;
-    inventory.datePurchase = req.body.datePurchase;
-    inventory.price = req.body.price;
-    inventory.userCharge = req.body.userCharge._id;
-    inventory.brand = req.body.brand._id;
-    inventory.computerStatus = req.body.computerStatus._id;
-    inventory.computerType = req.body.computerType._id;
-    inventory.updateDate = new Date();
+      let inventory = await Inventario.findById(req.params.inventarioId);
+      if (!inventory) {
+        return res.status(400).send("El inventario no existe");
+      }
+      const exitsInventorySerial = await Inventario.findOne({
+        serial: req.body.serial,
+        _id: { $ne: inventory._id },
+      });
+      if (exitsInventorySerial) {
+        return res.status(400).send("El serial existe para otro equipo");
+      }
+      inventory.serial = req.body.serial;
+      inventory.model = req.body.model;
+      inventory.description = req.body.description;
+      inventory.photoComputer = req.body.photoComputer;
+      inventory.color = req.body.color;
+      inventory.datePurchase = req.body.datePurchase;
+      inventory.price = req.body.price;
+      inventory.userCharge = req.body.userCharge._id;
+      inventory.brand = req.body.brand._id;
+      inventory.computerStatus = req.body.computerStatus._id;
+      inventory.computerType = req.body.computerType._id;
+      inventory.updateDate = new Date();
 
-    inventory = await inventory.save();
-    res.send(inventory);
-    console.log("Tipo de equipo actualizado con exito: ", inventory);
-  } catch (e) {
-    console.log(e);
-    res.status(500).send("Ocurrió un error");
+      inventory = await inventory.save();
+      res.send(inventory);
+      console.log("Tipo de equipo actualizado con exito: ", inventory);
+    } catch (e) {
+      console.log(e);
+      res.status(500).send("Ocurrió un error");
+    }
   }
-});
+);
 
-router.delete("/:inventarioId", [validarJWT, validarRolAdmin], async (req, res) => {
-  try {
-    //obtener usuario por id
-    let inventory = await Inventario.findById(req.params.inventarioId);
-    //Validar si existe
-    if (!inventory) {
-      return res.status(400).send("El inventario no existe");
+router.delete(
+  "/:inventarioId",
+  [validarJWT, validarRolAdmin],
+  async (req, res) => {
+    try {
+      //obtener usuario por id
+      let inventory = await Inventario.findById(req.params.inventarioId);
+      //Validar si existe
+      if (!inventory) {
+        return res.status(400).send("El inventario no existe");
+      }
+      console.log(inventory);
+
+      inventory = await inventory.remove();
+      res.send("El inventario se ha eliminado con exito");
+
+      res.send(inventory);
+    } catch (err) {
+      console.log(err);
+      res.status(500).send("Ocurrió un error");
     }
-    console.log(inventory);
-
-    inventory = await inventory.remove();
-    res.send("El inventario se ha eliminado con exito");
-
-    res.send(inventory);
-  } catch (err) {
-    console.log(err);
-    res.status(500).send("Ocurrió un error");
   }
-});
+);
 
 router.get("/:inventarioId", [validarJWT], async (req, res) => {
   try {
